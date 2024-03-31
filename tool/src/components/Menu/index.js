87046ccaf37a3636731729e606e8886dff5cc6af
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-01 02:17:19 +0530

    UI for toolbox

--- tool/src/components/Menu/index.js
+++ tool/src/components/Menu/index.js
@@ -1,7 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import styles from './index.module.css'
+
 const Menu = () => {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className={styles.menuContainer}>
+            <div>
+                <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
+            </div>
         </div>
     )
 }
