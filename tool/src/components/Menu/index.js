ac32a7222f0e1d6e9c4c2cde32fe747aafa46ead
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-01 23:52:37 +0530

    setting up redux toolkit

--- tool/src/components/Menu/index.js
+++ tool/src/components/Menu/index.js
@@ -1,15 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
+import {useDispatch} from 'react-redux'
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
+import { MENU_ITEMS } from '@/constants'
 
 const Menu = () => {
+    const dispatch = useDispatch()
+
+    const handleMenuClick = (itemName) => {
+        dispatch (menuItemClick(item))
+    }
+
     return (
         <div className={styles.menuContainer}>
-            <div>
-                <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+            <div className={styles.iconWrapper}>
+                <FontAwesomeIcon icon={faPencil} className={styles.icon} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)} />
             </div>
             <div>
-                <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+                <FontAwesomeIcon icon={faEraser} className={styles.icon} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}/>
             </div>
             <div>
                 <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
