6b28517ddda142a96c04ced74dc886b2c6e78b22
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-02 22:52:18 +0530

    toolbox and menu connected via redux toolkit

--- tool/src/components/Menu/index.js
+++ tool/src/components/Menu/index.js
@@ -1,12 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
 import { MENU_ITEMS } from '@/constants'
+import cx from 'classnames'
 
 const Menu = () => {
     const dispatch = useDispatch()
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
 
     const handleMenuClick = (itemName) => {
         dispatch (menuItemClick(item))
@@ -14,19 +16,19 @@ const Menu = () => {
 
     return (
         <div className={styles.menuContainer}>
+            <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+                <FontAwesomeIcon icon={faPencil} className={styles.icon}  />
+            </div>
+            <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+                <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+            </div> 
             <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faPencil} className={styles.icon} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)} />
-            </div>
-            <div>
-                <FontAwesomeIcon icon={faEraser} className={styles.icon} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}/>
-            </div>
-            <div>
                 <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
             </div>
-            <div>
+            <div className={styles.iconWrapper}>
                 <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
             </div>
-            <div>
+            <div className={styles.iconWrapper}>
                 <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
             </div>
         </div>
