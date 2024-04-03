85a5b9c5422ea0e88441f13e408c575b55bfd9c9
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-03 21:07:14 +0530

    toolboxSlice implemented

--- tool/src/components/Board/index.js
+++ tool/src/components/Board/index.js
@@ -1,7 +1,10 @@
 import { useEffect, useRef } from "react"
+import { useSelector, useDispatch} from 'react-redux'
 
 const Board = () => {
     const canvasRef = useRef(null)
+    const {activeMenuItem, actionMenuItem} = useSelector((state) => state.menu)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
 
     useEffect(() => {
         if(!canvasRef.current) return
